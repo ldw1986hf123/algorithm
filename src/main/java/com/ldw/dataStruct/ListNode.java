b0c14bb52446cb067baa7e942a262e08f6a81730
ldw1986hf123@163.com <19861020123abc>
2021-08-18 23:02:06 +0800

    将两个链表合并

---
+++ src/main/java/com/ldw/dataStruct/ListNode.java
@@ -0,0 +1,46 @@
+package com.ldw.dataStruct;
+
+
+/**
+ * 将两个链表合并为一个链表并返回，
+ * <p>
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode(int x) { val = x; }
+ * }
+ */
+
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+
+}
+
+class MergeTwoLickList {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode head = new ListNode(0);
+        ListNode temp = head;
+        while (l1 != null && l2 != null) {
+            if (l1.val > l2.val) {
+                temp.next = l2;
+                l2 = l2.next;
+            } else {
+                temp.next = l1;
+                l1 = l1.next;
+            }
+            temp = temp.next;
+        }
+        if (l1 != null) {
+            temp.next = l1;
+        } else {
+            temp.next = l2;
+        }
+        return head.next;
+    }
+}
